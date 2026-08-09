0eade877bd2ed00532ca5d6e4cd91d4c087eb01b
ChrisFelber <cfelber.bap@gmail.com>
2026-08-10 01:33:41 +0200

    Use shared auth helper from lib

--- api/admin-session.js
+++ api/admin-session.js
@@ -1,4 +1,4 @@
-import { isAuthenticated } from './_admin-auth.js';
+import { isAuthenticated } from '../lib/admin-auth.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
